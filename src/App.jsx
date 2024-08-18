158e7ad4f11aeb469294fe880b67098358b6572e
coder123332 <widnezz@gmail.com>
2024-08-18 03:53:44 -0700

    Separated ComicBubbles and BoxLogoGenerator components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useRef, useState } from "react";
 import "./App.css";
 import { toPng, toSvg } from 'html-to-image';
-import BoxLogoGenerator from '../Box Logo generator/BoxLogoGenerator';
 
 
 

1ee45ebb46aa3e838e2b0350d2561848410d441e
coder123332 <widnezz@gmail.com>
2025-05-05 16:22:20 -0700

    Update vite.config.js
    roll back

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/chatbubble-project/', // <-- this line is the fix
+  plugins: [react()],
 })
